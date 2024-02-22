9a6db748db82d76e665374bddd2fbfd849acf547
afaq-ahmed07 <afaqshahbaz9@gmail.com>
2024-02-22 21:50:00 +0500

    Services and Contact Page Added

--- Portfolio/assets/script/script.js
+++ Portfolio/assets/script/script.js
@@ -6,8 +6,6 @@ menuIcon.onclick = () =>{
     navbar.classList.toggle('active');
 }
 
-
-
 let sections = document.querySelectorAll('section');
 let navlinks = document.querySelectorAll('header nav a');
 
