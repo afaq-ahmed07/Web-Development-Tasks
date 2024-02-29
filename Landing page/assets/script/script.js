54b60718554c924c52e9ff5b27e62920a6c254c5
afaq-ahmed07 <afaqshahbaz9@gmail.com>
2024-03-01 01:22:20 +0500

    Landing Page  Changed

--- Landing page/assets/script/script.js
+++ Landing page/assets/script/script.js
@@ -1,3 +1,37 @@
+let menuIcon =document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () =>{
+    sections.forEach(sec =>{
+        let top=window.scrollY;
+        let offset= sec.offsetTop-150;
+        let height= sec.offsetHeight;
+        let id= sec.getAttribute('id');
+    if(top>= offset && top <offset +height){
+         navlinks.forEach(links => {
+            links.classList.remove('active');
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+         })
+    }
+
+    })
+
+
+    let header= document.querySelector('header');
+    header.classList.toggle('sticky',window.scrollY > 100);
+
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+  }
 document.addEventListener('DOMContentLoaded', function () {
     ScrollReveal({
       reset: true,
@@ -10,4 +44,3 @@ document.addEventListener('DOMContentLoaded', function () {
     ScrollReveal().reveal('.home-img, .service-container div, .about-brands div', { origin: 'right' });
     ScrollReveal().reveal('.home-content p, .services-content p, .about-content p, .learn-btn, .button, .contact', { origin: 'bottom' });
   });
-  
